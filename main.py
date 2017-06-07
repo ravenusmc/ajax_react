60742a66887f177f2142a98563e878c6697f25f3
ravenusmc <mcuddy77@gmail.com>
2017-06-07 14:39:32 -0400

    working on ajax call

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #Importing the required files 
 from flask import Flask
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_json import FlaskJSON
+import json
 
 #Setting up Flask
 app = Flask(__name__)
+json = FlaskJSON(app)
  
 #This function will bring the user to the home page
 @app.route("/")
@@ -23,16 +26,35 @@ def add_numbers():
 
 #The functions for num and add_num are a new way to apply ajax to my site. Thus, the reason for the repitiion. 
 
-@app.route("/num")
+@app.route("/num", methods=['GET', 'POST'])
 def num():
+    # c = 0
+    # if request.method == 'POST':
+    #     a = request.form['a']
+    #     b = request.form['b']
+    #     a = int(a)
+    #     b = int(b)
+    #     c = a + b
+    #     return redirect(url_for('num'))
+    # return render_template('num.html', c = c)
     return render_template('num.html')
 
 
-@app.route('/add_num')
+@app.route('/add_num', methods=['POST'])
 def add_num():
     a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+    b = request.args.get('a', 0, type=int)
+    print(a)
+    print(b)
+    # a = request.form['a']
+    # b = request.form['b']
+    # a = int(a)
+    # b = int(b)
+    # c = a + b
+    # print(c)
+    #return json.dumps({'status':'OK', 'c': c});
+    #return redirect(url_for('num'))
+    return jsonify(result = a + b)
 
 
 #This line will actually run the app.
