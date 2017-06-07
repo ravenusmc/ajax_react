32f8add3d174d240387f6350dd281736bce25c3a
ravenusmc <mcuddy77@gmail.com>
2017-06-07 07:51:17 -0400

    working on getting ajax to work

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+#This function is where I will be calculating the sum of the two numbers. 
 @app.route('/_add_numbers', methods=['GET', 'POST'])
 def add_numbers():
     a = request.form['a']
@@ -20,7 +21,20 @@ def add_numbers():
     # b = request.args.get('b', 0, type=int)
     # return jsonify(result=a + b)
 
- 
+#The functions for num and add_num are a new way to apply ajax to my site. Thus, the reason for the repitiion. 
+
+@app.route("/num")
+def num():
+    return render_template('num.html')
+
+
+@app.route('/add_num')
+def add_num():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+
 #This line will actually run the app.
 if __name__ == '__main__':
     app.run(debug=True)
